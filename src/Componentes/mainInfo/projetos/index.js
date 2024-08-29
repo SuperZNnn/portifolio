c536f479a220f2c9a14aa66e8c9877b04375ac0b
Rafael Soares <rafael.soares.pb@compasso.com.br>
2024-08-29 19:09:45 -0300

    Adicionado novo Projeto - Control It

--- src/Componentes/mainInfo/projetos/index.js
+++ src/Componentes/mainInfo/projetos/index.js
@@ -13,6 +13,14 @@ const ProjetosContainer = () => {
                 projectTecs={["HTML","CSS","JavaScript", "PHP"]}
                 />
             </a>
+            <a href="https://control-it.rafaelsoares.online" target="blank">
+                <ProjectCards
+                projectName="Control It"
+                projectImgSrc="assets/projectThumbs/controlit.png"
+                projectDescrip="Plataforma de apresentações"
+                projectTecs={["REACT.JS","PHP",]}
+                />
+            </a>
         </section>
     )
 }
